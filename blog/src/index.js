c32da7ff0fca06acd2f722aecf4c24c93d8215de
Liz Shaw <lizmhs@gmail.com>
2018-08-19 13:27:02 -0400

    Style pagination & add lazy-loading & some linting

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,8 +1,17 @@
 import "./sass/main.sass";
+import Blazy from "blazy";
 import toggleScrollClass from "./js/toggleScrollClass";
 import skrollr from "skrollr";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const articleImages = document.querySelectorAll(".post__article img");
+  [].forEach.call(articleImages, articleImage =>
+    articleImage.classList.add("lazy")
+  );
+  new Blazy({
+    selector: ".lazy",
+    offset: 400
+  });
   toggleScrollClass("header");
   skrollr.init();
 });
